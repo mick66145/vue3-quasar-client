4f114ee55bfe0ac7eb8f9592f119a954eb498070
ming <mick66145@gmail.com>
2024-10-15 22:50:07 +0800

    chore(tailwind): 預設css

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,63 +20,6 @@ export default {
           focus: 'var(--color-func-focus)',
         },
       },
-      typography: {
-        DEFAULT: {
-          css: {
-            a: {
-              'text-decoration': 'none',
-            },
-            hr: {
-              margin: 0,
-            },
-            img: {
-              margin: 0,
-            },
-            h1: {
-              'font-size': '4rem',
-              'line-height': '4rem',
-              'font-weight': 600,
-              'margin-top': '1rem',
-              'margin-bottom': '2rem',
-            },
-            h2: {
-              'font-size': '3.5rem',
-              'line-height': '3.5rem',
-              'font-weight': 600,
-              'margin-top': '1rem',
-              'margin-bottom': '2rem',
-            },
-            h3: {
-              'font-size': '2.75rem',
-              'line-height': '3rem',
-              'font-weight': 500,
-              'margin-top': '1rem',
-              'margin-bottom': '2rem',
-            },
-            h4: {
-              'font-size': '2.125rem',
-              'line-height': '2.5rem',
-              'font-weight': 500,
-              'margin-top': '1rem',
-              'margin-bottom': '2rem',
-            },
-            h5: {
-              'font-size': '1.5rem',
-              'line-height': '2rem',
-              'font-weight': 400,
-              'margin-top': '1rem',
-              'margin-bottom': '2rem',
-            },
-            h6: {
-              'font-size': '1.25rem',
-              'line-height': '2rem',
-              'font-weight': 400,
-              'margin-top': '1rem',
-              'margin-bottom': '2rem',
-            },
-          },
-        },
-      },
     },
     screens: {
       xs: '0',
