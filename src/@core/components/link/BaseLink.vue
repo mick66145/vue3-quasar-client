23b26fc6283e2fa7515fb1a675ecc838dae1275a
ming <mick66145@gmail.com>
2024-10-19 03:55:49 +0800

    feat(BaseLink): align left

--- src/@core/components/link/BaseLink.vue
+++ src/@core/components/link/BaseLink.vue
@@ -1,7 +1,9 @@
 <template>
   <q-btn
     flat
+    no-caps
     padding="0"
+    :align="align"
     :size="size"
     :color="color"
     :label="label"
@@ -14,22 +16,25 @@
 </template>
 
 <script>
-import { defineComponent, computed } from 'vue-demi'
+import { defineComponent, computed, toRefs } from 'vue-demi'
 
 export default defineComponent({
   props: {
     label: { type: String, default: '' },
     color: { type: String, default: 'primary' },
     size: { type: String },
+    align: { type: String, default: 'left' },
     underline: { type: Boolean, default: true },
     to: { type: String },
     href: { type: String },
     target: { type: String, default: '_blank' },
   },
   setup (props) {
+    const { underline } = toRefs(props)
+    // computed
     const observeClass = computed(() => {
       return {
-        underline: props.underline,
+        underline: underline.value,
       }
     })
     return {
