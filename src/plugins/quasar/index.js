96bc4074f6e2e6c692a745ffc2d1eab975b839ca
ming <mick66145@gmail.com>
2025-05-03 12:17:11 +0800

    chore(plugins): material-symbols

--- src/plugins/quasar/index.js
+++ src/plugins/quasar/index.js
@@ -7,6 +7,7 @@ import '@quasar/quasar-ui-qcalendar/src/QCalendarMonth.sass'
 import 'quasar/src/css/index.sass'
 import '@quasar/extras/material-icons/material-icons.css'
 import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
+import '@quasar/extras/material-symbols-outlined/material-symbols-outlined.css'
 
 export default function (app) {
   app.use(Quasar, {
